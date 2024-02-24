c3e0a77f55a002f230ea390d4131fd9603232cf5
Chiko but no H <franciscotrianto@gmail.com>
2024-02-24 14:47:42 +0700

    feat: initial bot starter

---
+++ src/bot/run-bots.bat
@@ -0,0 +1,5 @@
+@echo off
+start cmd /c "python main.py --logic Random --email=test@email.com --name=stima --password=123456 --team etimo"
+start cmd /c "python main.py --logic Random --email=test1@email.com --name=stima1 --password=123456 --team etimo"
+start cmd /c "python main.py --logic Random --email=test2@email.com --name=stima2 --password=123456 --team etimo"
+start cmd /c "python main.py --logic Random --email=test3@email.com --name=stima3 --password=123456 --team etimo"
---
+++ src/bot/run-bots.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+python main.py --logic Random --email=test@email.com --name=stima --password=123456 --team etimo &
+python main.py --logic Random --email=test1@email.com --name=stima1 --password=123456 --team etimo &
+python main.py --logic Random --email=test2@email.com --name=stima2 --password=123456 --team etimo &
+python main.py --logic Random --email=test3@email.com --name=stima3 --password=123456 --team etimo &
