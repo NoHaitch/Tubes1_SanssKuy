64540293df4df07286fc48c62537ecb1f61b835b
Chiko but no H <franciscotrianto@gmail.com>
2024-03-01 23:52:42 +0700

    feat: move ignore going into portal

--- src/bot/game/logic/botGreedyPath.py
+++ src/bot/game/logic/botGreedyPath.py
@@ -12,9 +12,17 @@ import time
 
 class BotGreedyPath(BaseLogic):
     def __init__(self):
-        self.stepVariation = 0
-        self.stepChaseEnemy = 0
+        self.step_variation : bool = False
+        self.step_ignore_portal : int = 0
+        self.step_chase_enemy : int = 0
+        self.current_distance_to_base : int = 0
+        self.current_inventory_space : int = None
 
+        # self.self.sorted_portals[0] is the portal closest to player, while [1] is further
+        self.sorted_portals : list[GameObject] = []
+        # self.self.distance_self_to_portals[0] is the distant to the portal closest to player, while [1] is further
+        self.distance_self_to_portals : list[int] = []
+        self.base : GameObject = None
 
     # ==================== GETTER ==================== #
     def getBots(self, board: Board) -> list[GameObject]: return board.bots
@@ -30,15 +38,23 @@ class BotGreedyPath(BaseLogic):
     def getRedDiamonds(self, board: Board) -> list[GameObject]: return [diamond for diamond in board.diamonds if diamond.properties.points == 2]
     def getBlueDiamonds(self, board: Board) -> list[GameObject]: return [diamond for diamond in board.diamonds if diamond.properties.points == 1]
     
-    def getPortals(self, board: Board) -> list[GameObject]: return [home for home in board.game_objects if home.type=="TeleportGameObject"]
+    def getPortals(self, board: Board) -> list[GameObject]: return [portal for portal in board.game_objects if portal.type=="TeleportGameObject"]
     def getSortedPortals(self, this_bot: GameObject, board: Board) -> list[GameObject]: 
         # Sort portals based on distance to bot
         portals = self.getPortals(board)
-        if(self.distanceWithoutPortal(this_bot, portals[0]) < self.distanceWithoutPortal(this_bot, portals[1])):
-            return portals[0], portals[1]
+        if portals:
+            if self.distanceWithoutPortal(this_bot, portals[0]) < self.distanceWithoutPortal(this_bot, portals[1]):
+                return portals[0], portals[1]
+            else:
+                return portals[1], portals[0]
+        return None
+    def getSortedPortalsDistance(self, this_bot: GameObject, board: Board) -> list[int]:
+        if self.sorted_portals:
+            return (self.distance(this_bot, self.sorted_portals[0], board), self.distance(this_bot, self.sorted_portals[1], board))
         else:
-            return portals[1], portals[0]
-    def getDiamondButton(self, board: Board) -> GameObject: return [home for home in board.game_objects if home.type=="DiamondButtonGameObject"][0]
+            return (-1, -1)
+
+    def getDiamondButton(self, board: Board) -> GameObject: return [button for button in board.game_objects if button.type=="DiamondButtonGameObject"][0]
 
     def getClosestRedDiamond(self, this_bot: GameObject, board: Board) -> GameObject:
         red_diamonds = self.getRedDiamonds(board)
@@ -67,21 +83,22 @@ class BotGreedyPath(BaseLogic):
     def getTimeRemaining(self, this_bot:GameObject) -> int: return this_bot.properties.milliseconds_left 
 
     # ==================== Checks ==================== #
-    def isInventoryFull(self, this_bot: GameObject) -> bool: return this_bot.properties.diamonds == this_bot.properties.inventory_size
-    def isInventoryEmpty(self, this_bot: GameObject) -> bool: return this_bot.properties.diamonds == 0
+    def isInventoryFull(self) -> bool: return self.current_inventory_space == 0
+    def isInventoryEmpty(self, this_bot: GameObject) -> bool: return self.current_inventory_space == this_bot.properties.inventory_size
 
     # ==================== Distance ===================== #
-    def distance(self, objectFrom: GameObject, objectTo: GameObject, board: Board) -> int: return min(self.distanceWithoutPortal(objectFrom, objectTo), self.distanceUsingPortal(objectFrom, objectTo, board))
+    def distance(self, objectFrom: GameObject, objectTo: GameObject, board: Board) -> int: 
+        if objectTo.type =="TeleportGameObject": return self.distanceWithoutPortal(objectFrom, objectTo)
+        return min(self.distanceWithoutPortal(objectFrom, objectTo), self.distanceUsingPortal(objectFrom, objectTo, board))
     def distanceWithoutPortal(self, objectFrom: GameObject, objectTo: GameObject) -> int: return abs(objectFrom.position.y - objectTo.position.y) + abs(objectFrom.position.x - objectTo.position.x)
     def distanceUsingPortal(self, objectFrom: GameObject, objectTo: GameObject, board: Board) -> int:
-        portals = self.getSortedPortals(objectFrom, board)
-        return self.distanceWithoutPortal(objectFrom, portals[0]) + self.distanceWithoutPortal(portals[1], objectTo)
+        return self.distanceWithoutPortal(objectFrom, self.sorted_portals[0]) + self.distanceWithoutPortal(self.sorted_portals[1], objectTo)
     def getClosestEnemyDistance(self, this_bot: GameObject, board: Board) -> int: 
         enemy = self.getClosestEnemy(this_bot, board)
         if not enemy:
             return -1
         return self.distance(this_bot, self.getClosestEnemy(this_bot, board), board)
-    def getBaseDistance(self, this_bot: GameObject, board: Board) -> int: return self.distance(this_bot, self.getHomeBaseObject(this_bot, board), board)
+    def getBaseDistance(self, this_bot: GameObject, board: Board) -> int: return self.distance(this_bot, self.base, board)
 
     # ==================== Movement ==================== #
     def moveRight(self): return (1,0)
@@ -93,8 +110,9 @@ class BotGreedyPath(BaseLogic):
         x_diff:int = this_bot.position.x - objective.position.x
         y_diff:int = this_bot.position.y - objective.position.y
 
-        if objective.type=="TeleportGameObject" and self.distance(this_bot, self.getHomeBaseObject(this_bot, board), board) == 0:
-            temp = randint(1,4)
+        # After Teleport
+        if objective.type=="TeleportGameObject" and self.distance(this_bot, self.base, board) == 0:
+            temp:int = randint(1,4)
             if temp == 1:
                 return self.moveUp(this_bot, board)
             elif temp == 2:
@@ -104,27 +122,63 @@ class BotGreedyPath(BaseLogic):
             else:
                 return self.moveRight(this_bot, board)
 
-        if objective.type!="TeleportGameObject" and self.distance(this_bot, self.getHomeBaseObject(this_bot, board), board) > self.distanceUsingPortal(this_bot, self.getHomeBaseObject(this_bot, board), board):
-            return self.moveToObjective(this_bot, self.getSortedPortals(this_bot, board)[0], board)
+        # Go To Portal if faster
+        if objective.type!="TeleportGameObject" and self.distance(this_bot, self.base, board) > self.distanceUsingPortal(this_bot, self.base, board):
+            print("PORTAL is closer!")
+            return self.moveToObjective(this_bot, self.sorted_portals[0], board)
 
-        if(self.stepVariation % 2 == 0):
-            self.stepVariation += 1
-            if x_diff < 0: return self.moveRight()
-            elif x_diff > 0: return self.moveLeft()
-            elif y_diff > 0: return self.moveDown()
-            elif y_diff < 0: return self.moveUp()
+        if objective.type!="TeleportGameObject":
+            # self.distance_self_to_portals[0] is the distance to the nearest portal
+            if self.distance_self_to_portals[0] == 1:
+                print("TRYING TO IGNORE PORTAL")
+                x_portal_diff:int  = this_bot.position.x - self.sorted_portals[0].position.x
+                y_portal_diff:int  = this_bot.position.y - self.sorted_portals[0].position.y
+
+                # because portal is near make sure to avoid it
+                if x_diff < 0 and x_portal_diff == -1 : 
+                    self.step_ignore_portal = 1
+                    if y_diff < 0 : return self.moveUp()
+                    else : return self.moveDown()
+                    
+                elif x_diff > 0 and x_portal_diff == 1 : 
+                    self.step_ignore_portal = 2
+                    if y_diff < 0 : return self.moveUp()
+                    else : return self.moveDown()
+                    
+                elif y_diff < 0 and y_portal_diff == -1 : 
+                    self.step_ignore_portal = 3
+                    if x_diff < 0 : return self.moveRight()
+                    else : return self.moveLeft()
+
+                elif y_diff > 0 and y_portal_diff == 1 : 
+                    self.step_ignore_portal = 4
+                    if x_diff < 0 : return self.moveRight()
+                    else : return self.moveLeft()
+
+        if self.step_ignore_portal == 1: self.step_ignore_portal = 0; print("IGNORE PORTAL Follow up"); return self.moveRight()
+        elif self.step_ignore_portal == 2: self.step_ignore_portal = 0; print("IGNORE PORTAL Follow up"); return self.moveLeft()
+        elif self.step_ignore_portal == 3: self.step_ignore_portal = 0; print("IGNORE PORTAL Follow up"); return self.moveUp()
+        elif self.step_ignore_portal == 4: self.step_ignore_portal = 0; print("IGNORE PORTAL Follow up"); return self.moveDown()
+
+        # Move directly toward objective
+        if self.step_variation :
+            self.step_variation = False
+            if x_diff < 0 : return self.moveRight()
+            elif x_diff > 0 : return self.moveLeft()
+            elif y_diff < 0 : return self.moveUp()
+            elif y_diff > 0 : return self.moveDown()
         else:
-            self.stepVariation -= 1
-            if y_diff > 0: return self.moveDown()
-            elif y_diff < 0: return self.moveUp()
-            elif x_diff < 0: return self.moveRight()
-            elif x_diff > 0: return self.moveLeft()
-                
+            self.step_variation = True
+            if y_diff < 0 : return self.moveUp()
+            elif y_diff > 0 : return self.moveDown()
+            elif x_diff < 0 : return self.moveRight()
+            elif x_diff > 0 : return self.moveLeft()
+
         return None
     def moveToBase(self, this_bot: GameObject, board: Board):
-        if self.distanceWithoutPortal(this_bot, self.getHomeBaseObject(this_bot, board)) > self.distanceUsingPortal(this_bot, self.getHomeBaseObject(this_bot, board), board):
-            return self.moveToObjective(this_bot, self.getSortedPortals(this_bot, board)[0], board)
-        return self.moveToObjective(this_bot, self.getHomeBaseObject(this_bot, board), board)
+        if self.distanceWithoutPortal(this_bot, self.base) > self.distanceUsingPortal(this_bot, self.base, board):
+            return self.moveToObjective(this_bot, self.sorted_portals[0], board)
+        return self.moveToObjective(this_bot, self.base, board)
     def movetoClosestEnemy(self, this_bot: GameObject, board: Board): return self.moveToObjective(this_bot, self.getClosestEnemy(this_bot, board), board)
     
     # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
@@ -145,29 +199,38 @@ class BotGreedyPath(BaseLogic):
     
     def next_move(self, this_bot: GameObject, board: Board):
     # BOT FUNCTION CALL BY ENGINE
+        # Static var
+        if not self.base:
+            self.base = self.getHomeBaseObject(this_bot, board)
+
+        # Dynamic Var
+        self.sorted_portals = self.getSortedPortals(this_bot, board)
+        self.distance_self_to_portals = self.getSortedPortalsDistance(this_bot, board)
+        self.current_distance_to_base = self.getBaseDistance(this_bot, board)
+        self.current_inventory_space = self.getEmptyInventorySpace(this_bot)
 
     # 1 If timeleft is less than the distance to home, then go home
-        if not self.isInventoryEmpty(this_bot) and (self.getTimeRemaining(this_bot) // 1000) + 4 <= self.getBaseDistance(this_bot, board):
-            print("LOW TIME - GOING HOME")
+        if not self.isInventoryEmpty(this_bot) and (self.getTimeRemaining(this_bot) // 1000) - 2 <= self.current_distance_to_base:
+            # print("LOW TIME - GOING HOME")
             return self.moveToBase(this_bot, board)
 
     # 2 Check for near Enemy 
-        if self.getClosestEnemyDistance(this_bot, board) == 1 and self.stepChaseEnemy <= 2:
-            print("Attack Close Enemy.")
-            self.stepChaseEnemy += 1
-            return self.movetoClosestEnemy(this_bot, board)
-        elif self.stepChaseEnemy == 4:
-            self.stepChaseEnemy = 0
-        elif self.stepChaseEnemy < 2:
-            self.stepChaseEnemy += 1
+        if self.getClosestEnemyDistance(this_bot, board) == 1 and self.step_chase_enemy <= 2:
+            # print("Attack Close Enemy.")
+            self.step_chase_enemy += 1
+            return self.movetoClosestEnemy(this_bot, board) 
+        elif self.step_chase_enemy == 4:
+            self.step_chase_enemy = 0
+        elif self.step_chase_enemy < 2:
+            self.step_chase_enemy += 1
 
     # 3 Check inventory full    
-        if self.isInventoryFull(this_bot):
+        if self.isInventoryFull():  
             # print("Inventory Full.")
             return self.moveToBase(this_bot, board)
 
     # 4 If inventory is half full but the base is near, deposit the points
-        if self.getBaseDistance(this_bot, board) <= 2 and self.getUsedInventorySpace(this_bot) >= 3:
+        if self.current_distance_to_base <= 2 and self.current_inventory_space < 3:
             return self.moveToBase(this_bot, board)
 
     # 5 Go to the diamond that is within 2 moves and have sufficient inventory
@@ -179,13 +242,13 @@ class BotGreedyPath(BaseLogic):
                 return self.moveToObjective(this_bot, self.getDiamondButton(board), board)
         
     # 6 Go to nearest diamond if inventory space is more than equal 2   
-        if closest_diamond and self.getEmptyInventorySpace(this_bot) >= 2:
+        if closest_diamond and self.current_inventory_space >= 2:
             # print(f"Going to Diamond. Location : {closest_diamond.position}")
             return self.moveToObjective(this_bot, closest_diamond, board) 
             
     # 7 Go to blue diamond if sufficient inventory and distance is within 2 moves
         closest_diamond = self.getClosestBlueDiamond(this_bot, board)
-        if closest_diamond and self.getEmptyInventorySpace(this_bot) == 1 and closest_diamond.properties.points == 1 and diamond_distance <= 2:
+        if closest_diamond and self.current_inventory_space == 1 and closest_diamond.properties.points == 1 and diamond_distance <= 2:
             # print(f"Going to Diamond. Location : {closest_diamond.position}")
             return self.moveToObjective(this_bot, closest_diamond, board) 
 
--- src/bot/run-bots.bat
+++ src/bot/run-bots.bat
@@ -1,5 +1,5 @@
 @echo off
-start cmd /c "python main.py --logic BotGreedyPath --email=greedy1@email.com --name=gpath --password=123456 --team etimo"
-start cmd /c "python main.py --logic BotGreedyPoints --email=greedy2@email.com --name=gpoints --password=123456 --team etimo"
+start cmd /c "python main.py --logic BotGreedyPath --email=test6@email.com --name=main --password=123456 --team etimo"
+start cmd /c "python main.py --logic BotGreedyPoints --email=test7@email.com --name=gpoints --password=123456 --team etimo"
 start cmd /c "python main.py --logic BotChase --email=chase@email.com --name=chase --password=123456 --team etimo"
 start cmd /c "python main.py --logic Random --email=random2@email.com --name=random2 --password=123456 --team etimo"
