064f08f7fd2e09838382f278a977ccbcd665fcc9
Chiko but no H <franciscotrianto@gmail.com>
2024-03-08 20:51:30 +0700

    feat: update sh

--- src/run-bots.sh
+++ src/run-bots.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python main.py --logic Random --email=test@email.com --name=stima --password=123456 --team etimo &
-python main.py --logic Random --email=test1@email.com --name=stima1 --password=123456 --team etimo &
-python main.py --logic Random --email=test2@email.com --name=stima2 --password=123456 --team etimo &
-python main.py --logic Random --email=test3@email.com --name=stima3 --password=123456 --team etimo &
+python main.py --logic MyBot --email=example1@email.com --name=main1 --password=123456 --team etimo &
+python main.py --logic MyBot --email=example2@email.com --name=main2 --password=123456 --team etimo &
+python main.py --logic Chase --email=example3@email.com --name=chase --password=123456 --team etimo &
+python main.py --logic Points --email=example4@email.com --name=points --password=123456 --team etimo &
